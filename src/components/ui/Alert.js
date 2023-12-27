21c731cde1388a88ef36fdad4712a45d60d7d462
Bruno <bruno.gomespeixoto1@gmail.com>
2023-12-27 12:19:45 +0100

    front: new Alert component

---
+++ src/components/ui/Alert.js
@@ -0,0 +1,48 @@
+import clsx from "clsx"
+import {
+  ExclamationCircleIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/24/outline"
+import { useEffect, useState } from "react"
+
+const variants = {
+  info: {
+    icon: InformationCircleIcon,
+    style: "bg-indigo-400 text-white",
+  },
+  error: {
+    icon: ExclamationCircleIcon,
+    style: "bg-red-500 text-white",
+  },
+}
+const Alert = (props) => {
+  const { children, variant = "info", className, ...otherProps } = props
+  const [displayAlert, setDisplayAlert] = useState(true)
+  const Icon = variants[variant].icon
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setDisplayAlert(false), 2500)
+
+    return () => clearTimeout(timeoutId)
+  }, [])
+
+  if (!displayAlert) {
+    return null
+  }
+
+  return (
+    <div
+      className={clsx(
+        "flex items-center w-fit gap-2 p-2 rounded-lg font-medium shadow-md shadow-neutral-200",
+        variants[variant].style,
+        className,
+      )}
+      {...otherProps}
+    >
+      <Icon className="w-6 h-6" />
+      <p>{children}</p>
+    </div>
+  )
+}
+
+export default Alert
